348c29e5b5ba0b89982400b6ee2970d2465fc5c8
Heather Drattlo <hdrattlo88@yahoo.com>
2020-04-27 17:21:03 -0700

    added body-parser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -15,8 +16,9 @@ const forceSSL = function() {
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL()); //enable this once ssl has been set up.
-app.use(express.urlencoded());
+// app.use(forceSSL()); //enable this once ssl has been set up.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,11 +26,11 @@ app.use(function (req, res, next) {
   next();
 });
 app.use(cors());
-app.use(express.static('/dist/thedariFair'));
+app.use(express.static('dist/thedariFair'));
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname,'dist/thedariFair/index.html'));
 });
-console.log(express.static('dist/thedariFair'));
+
 
 app.listen(process.env.PORT || 8888, function() {
   console.log('Listening on port ' + this.address().port);
